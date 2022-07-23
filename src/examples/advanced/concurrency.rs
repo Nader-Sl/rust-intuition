ed7ea438c307f79778a8c714a88c9ad7dc86aabd
Nader Sleiman <nader.sleiman@gmail.com>
2022-07-23 18:43:16 +0300

    Added condvars

--- src/examples/advanced/concurrency.rs
+++ src/examples/advanced/concurrency.rs
@@ -35,7 +35,7 @@ pub fn threading() {
 }
 
 mod sync_primitives {
-    use std::thread::JoinHandle;
+    use std::{borrow::BorrowMut, thread::JoinHandle};
 
     use super::*;
 
@@ -75,7 +75,7 @@ mod sync_primitives {
         let barrier = Arc::new(Barrier::new(THREADS_N));
 
         for _ in 0..THREADS_N {
-            let barrier = Arc::clone(&barrier); //clone the Arc to increase the strong ref count.
+            let barrier = Arc::clone(&barrier); //clone the Arc so it can be access by multiple threads.
 
             thread_handles.push(thread::spawn(move || {
                 println!("Hello Rust Fans!");
@@ -122,11 +122,11 @@ mod sync_primitives {
 
         const STACK_SIZE: usize = 10;
 
-        let thread_safe_stack = Arc::new(Mutex::new(Vec::<String>::with_capacity(STACK_SIZE)));
+        let stack_ref = Arc::new(Mutex::new(Vec::<String>::with_capacity(STACK_SIZE)));
 
         let mut thread_handles = Vec::<JoinHandle<_>>::with_capacity(2); // storage for the two threads handles.
 
-        let stack = Arc::clone(&thread_safe_stack); //clone the Arc to increase ref count so its not dropped on move.
+        let stack_ref_ = Arc::clone(&stack_ref); //clone the Arc so it can be access by multiple threads.
 
         // Create a thread to push a value onto the stack every 10 milliseconds.
         thread_handles.push(thread::spawn(move || {
@@ -139,7 +139,7 @@ mod sync_primitives {
                 // We need to check if there's been a mutex poisioning caused by a panic while the stack lock is being held in another
                 // thread, if so we can choose to panic here, return, or just continue, we'll just continue for now.
 
-                let mut stack = match stack.lock() {
+                let mut stack = match stack_ref_.lock() {
                     Ok(guard) => guard, // we just return the guard.
                     Err(poisoned) =>  {
                         // Poisioned mutex handling.
@@ -153,13 +153,13 @@ mod sync_primitives {
             }
         }));
 
-        let stack = Arc::clone(&thread_safe_stack); //clone the Arc to increase ref count so its not dropped on move.
+        let stack_ref_ = Arc::clone(&stack_ref); //clone the Arc so it can be access by multiple threads.
 
         // Create another thread to attempt to pop the values off the stack every 20 milliseconds.
         thread_handles.push(thread::spawn(move || {
             // Acquire the lock on the stack, which will block the thread until the lock (underlying resource) is available.
             for i in 0..STACK_SIZE {
-                if let Some(str) = stack.lock().unwrap().pop() {
+                if let Some(str) = stack_ref_.lock().unwrap().pop() {
                     if i == 2 {
                         // We intentionally panic on 2nd iteration to test the mutex posioning handler in the pushing thread.
                         panic!("The Mutex is now Posioned!");
@@ -180,8 +180,86 @@ mod sync_primitives {
     }
 
     #[test]
-    pub fn condvar_sync() {
-        //TODO
+    #[allow(unused_must_use)]
+    pub fn condvars() {
+        // Conditional Variables.
+
+        // Condition variables represent the ability to block a thread such that it consumes no CPU time while waiting for an event to occur.
+        // condvars are typically associated with a boolean predicate (a condition) and a mutex. The predicate is always verified
+        // inside of the mutex before determining that a thread must block.
+        // Functions in this module will block the current thread of execution. Note that any attempt to use multiple mutexes on the same
+        // condition variable may result in a runtime panic.
+
+        //== Example ==
+        // Let's take the example used in 'mutexes' test and modify it a little to demonstrate the condvars.
+        // Suppose we have two threads, one that pushes onto and another that pops off a stack, this time
+        // we only want to start popping off the stack once the pushing thread is done with its work.
+        // Although this seems like we don't require the use of a mutex since in this way we can guarantee that
+        // there won't be any data race condition having one thread write at a time, and the other read only after
+        // the latter has finished writing, but we still have to use a mutex to access the interior mutability pattern
+        // offered by a mutex, as the compiler will not allow the use of RefCell in multithreading context for extra safety.
+
+        use std::sync::{Arc, Condvar, Mutex};
+
+        let stack_ref = Arc::new(Mutex::new(Vec::<String>::new()));
+
+        // We need 2 synchronization primitives for the cross-thread notification mechanism to work, a Mutex (done_mutex) and a Condvar (cvar).
+        // The mutex will be used to wrap a bool (done) which will be set to true once the pushing thread is done. and the condvar will be used
+        // to notify (awaken) all the blocked threads waiting to be notified to resume.
+        let notify_prims_ref = Arc::new((Mutex::new(false), Condvar::new()));
+
+        let mut thread_handles = Vec::<JoinHandle<_>>::with_capacity(2); // storage for the two threads handles.
+
+        let stack_ref_ = Arc::clone(&stack_ref); //clone the Arc so it can be access by multiple threads.
+
+        let notify_prims_ref_ = Arc::clone(&notify_prims_ref); //clone the Arc so it can be access by multiple threads.
+
+        // Create a thread to push a value onto the stack.
+        thread_handles.push(thread::spawn(move || {
+            for i in 0..8 {
+                // push 8 values onto the stack.
+
+                let str = "String#".to_string() + &i.to_string();
+
+                stack_ref_.lock().unwrap().push(str.clone()); // Now that the resource is free, push a string.
+
+                println!("Pushed : {}", str);
+
+                thread::sleep(Duration::from_millis(10)); // Sleep 10 millis between pushes.
+            }
+
+            // Now that the thread is done pushing values onto the stack, notify the popping thread that it can start popping values off the stack.
+
+            // Destructure the done_mutex(Mutex) and cvar(Condvar) by reference (&) from dereferenced (*) notify_prims_ref_ (Arc).
+            let (done_mutex, cvar) = &*notify_prims_ref_;
+            let mut done = done_mutex.lock().unwrap();
+            *done = true; // Set the done bool to true by re-assigning its dereferenced value.
+            cvar.notify_one(); // Notify the popping thread that it can start popping values off the stack.
+                               // ** we can also use cvar.notify_all() to notify all the blocked threads if we have more than one.
+        }));
+
+        let stack_ref_ = Arc::clone(&stack_ref); //clone the Arc so it can be access by multiple threads.
+
+        let notify_prims_ref_ = Arc::clone(&notify_prims_ref); //clone the Arc so it can be access by multiple threads.
+                                                               // Create a thread to pop the values off the stack.
+        thread_handles.push(thread::spawn(move || {
+            // Wait for the pushing thread to finish pushing values onto the stack without consuming unneeded cpu in a conventional loop.
+
+            let (done_mutex, cvar) = &*notify_prims_ref_;
+ 
+            // Wait for the pushing thread to finish pushing values onto the stack.
+            cvar.wait(done_mutex.lock().unwrap());
+            println!("The pushing thread seems to be done, let's pop the stack");
+            while let Some(str) = stack_ref_.lock().unwrap().pop() {
+                // Pop off the stack until there are no more values to pop.
+                println!("Popped  {}", str);
+            }
+        }));
+
+        // Wait for other threads to finish before returning the test by joining the stored handles.
+        for handle in thread_handles {
+            handle.join().unwrap();
+        }
     }
     #[test]
     pub fn once_sync() {
